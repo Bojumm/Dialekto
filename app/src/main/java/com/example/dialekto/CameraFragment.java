fbb4ac72986ff0c6b2067e6c29b26ddd847c1fab
joshu <joshuafetalcurin06@gmail.com>
2024-09-14 00:16:08 +0800

    Final Commit

---
+++ app/src/main/java/com/example/dialekto/CameraFragment.java
@@ -0,0 +1,20 @@
+package com.example.dialekto;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CameraFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getActivity().setTitle("Camera");
+        View view = inflater.inflate(R.layout.camera_fragment, null);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/dialekto/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.dialekto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+    ChipNavigationBar chipNavigationBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chipNavigationBar = findViewById(R.id.bottom_nav);
+        chipNavigationBar.setItemSelected(R.id.bottom_nav, true);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+
+                switch(i){
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_voice:
+                        fragment = new VoiceFragment();
+                        break;
+                    case R.id.nav_camera:
+                        fragment = new CameraFragment();
+                        break;
+                    case R.id.nav_fave:
+                        fragment = new FavoriteFragment();
+                        break;
+                    default:
+                        // Log or handle the unexpected case
+                        Log.e("MainActivity", "Unknown item ID selected: " + i);
+                        return; // Exit the method if fragment is null
+                }
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+                } else {
+                    Log.e("MainActivity", "Fragment is null for item ID: " + i);
+                }
+            }
+        });
+    }
+}
