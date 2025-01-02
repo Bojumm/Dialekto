052355967696dea5390b5d6799b44410bb0f5511
hoyjaren <jarentabutabo12345@gmail.com>
2025-01-02 16:44:36 +0800

    done in signup page

--- app/src/main/java/com/example/dialekto/SignUp.java
+++ app/src/main/java/com/example/dialekto/SignUp.java
@@ -2,7 +2,6 @@ package com.example.dialekto;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,19 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
-    private TextView signUp;
-
+    private TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
-        signUp = findViewById(R.id.textView_signUp);
-
-        signUp.setOnClickListener(view -> {
-            Intent i = new Intent(getApplicationContext(), SignUp.class);
+        login = findViewById(R.id.textView_login);
+        login.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(), Login.class);
             startActivity(i);
         });
 
